db651b7aa7020703aae90f1e4fd2d73192e4aca2
jsabbae2702 <jsabbae2702@g.educaand.es>
2024-10-04 10:20:07 +0200

    04/10/2024

--- tema_03/ejemplos/03/index.php
+++ tema_03/ejemplos/03/index.php
@@ -3,19 +3,19 @@
  * Calificación de una nota de 0 a 10
  * Mosrtará: deficiente, insuficiente, suficiente, bien, notable o sobresaliente
  */
-$a = 11;
+$a = 7;
 
-if ($a >= 0 && $a < 3) {
+if ($a < 2) {
     echo "Deficiente";
-} else if ($a >= 3 && $a < 5) {
+} else if ($a < 5) {
     echo "Insuficiente";
-} else if ($a == 5) {
+} else if ($a < 6) {
     echo "Suficiente";
-} else if ($a == 6) {
+} else if ($a < 7) {
     echo "Bien";
-} else if ($a > 6 && $a < 10) {
+} else if ($a < 9) {
     echo "Notable";
-} elseif ($a == 10) {
+} elseif ($a <= 10) {
     echo "Sobresaliente";
 } else {
     echo "Calificación no permitida";
