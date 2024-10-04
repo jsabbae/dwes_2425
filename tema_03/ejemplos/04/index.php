38020faa3051396d53a2a05adc67efda34399684
jsabbae2702 <jsabbae2702@g.educaand.es>
2024-10-04 10:40:03 +0200

    04/10/2024

---
+++ tema_03/ejemplos/04/index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Estructura if corta o abreviada
+ */
+$a = 5;
+$b = 6;
+
+echo ($a > $b) ?$a:$b;
+
+?>
---
+++ tema_03/ejemplos/05/index.php
@@ -0,0 +1,79 @@
+
+<!-- 
+ejemplo 5
+Uso vistas 
+-->
+
+<!doctype html>
+<html lang="es">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Plantilla Bootstrap 5.3.2</title>
+
+    <!-- Bootstrap css -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+    <!-- Bootstrap Icons 1.11.1 -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+</head>
+
+<body>
+    <!-- Capa Principal -->
+    <div class="container">
+
+
+        <!-- Menú Principal -->
+
+        <ul class="nav">
+            <li class="nav-item">
+                <a class = "nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item">
+                <a class = "nav-link" <?= ($perfil  = editor)></a>
+            </li>
+            <li class="nav-item">
+                <a class = "nav-link" href="#"></a>
+            </li>
+            <li class="nav-item">
+                <a class = "nav-link" href="#"></a>
+            </li>
+            <li class="nav-item">
+                <a class = "nav-link" href="#"></a>
+            </li>
+            <li class="nav-item">
+                <a class = "nav-link" href="#"></a>
+            </li>
+            <li class="nav-item">
+                <a class = "nav-link" href="#"></a>
+            </li>
+        </ul>
+
+
+        <header class="pb-3 mb-4 border-bottom">
+            <i class="bi bi-app-indicator"></i>
+            <span class="fs-3">Vista Bootstrap</span>
+        </header>
+
+        <h1>Hola Bootstrap 5.3.2</h1>
+
+    </div>
+
+
+    <!-- Pie del documento -->
+    <footer class="footer mt-auto py-3 fixed-bottom bg-light">
+        <div class="container">
+            <span class="text-muted">© 2024
+                Juan Manel Saborido Baena - DWES - 2º DAW - Curso 24/25</span>
+        </div>
+    </footer>
+
+    <!-- Bootstrap Javascript y popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
