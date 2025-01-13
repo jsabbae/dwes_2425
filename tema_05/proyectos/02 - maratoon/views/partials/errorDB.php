9e1b0a28b4fc5b0b828972911644583770ea4309
juanma785 <jsabbae2702@g.educaand.es>
2025-01-13 19:09:25 +0100

    Completado

--- tema_05/proyectos/02 - maratoon/views/partials/errorDB.php
+++
@@ -1,7 +0,0 @@
-<h2>ERROR BASE DE DATOS</h2>
-<HR>;
-<?= "Mensaje:      " . $e->getMessage() . '<BR>' ?>
-<?= "Código:       " . $e->getCode() . '<BR>' ?>
-<?= "Fichero:      " . $e->getFile() . '<BR>' ?>
-<?= "Línea:        " . $e->getLine() . '<BR>' ?>
-<?= "Trace:        " . $e->getTraceAsString() . '<BR>' ?>
